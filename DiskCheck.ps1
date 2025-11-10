bb36faecb58f21603ef41b24df6d343b429ba78c
Abdul Bari <32057230+exabdulbari@users.noreply.github.com>
2025-11-11 05:30:36 +0800

    Update DiskCheck.ps1

--- DiskCheck.ps1
+++ DiskCheck.ps1
@@ -3,7 +3,7 @@
 
 Get-PSDrive -PSProvider FileSystem | ForEach-Object {
     $freeGB = [math]::Round($_.Free/1GB,2)
-    $totalGB = [math]::Round($_.Used/1GB + $freeGB,2)
+    $totalGB = [math]::Round(($_.Used + $_.Free)/1GB,2)
     Write-Output "Drive $($_.Name): $freeGB GB free out of $totalGB GB total"
 }
 
